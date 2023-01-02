8fbaaf7889178b3443703056829ef6a653f84135
manipapan2 <105167359+manipapan2@users.noreply.github.com>
2023-01-02 17:03:54 +0330

    Updated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var nav = document.getElementById("nav")
 var khatclick = document.getElementById("khatclick")
 var navmobile = document.getElementById("navmobile")
-var zabdar = document.getElementById("zabdar")
+var zabdarclick = document.getElementById("zabdarclick")
 window.addEventListener('load', (event) =>{
 
     nav.style.transform="translateY(0px)"
@@ -12,6 +12,6 @@ khatclick.addEventListener("click",() => {
     navmobile.style.transform = "translateX(0%)"
 })
 
-zabdar.addEventListener("click",() => {
+zabdarclick.addEventListener("click",() => {
     navmobile.style.transform = "translateX(100%)"
 })
